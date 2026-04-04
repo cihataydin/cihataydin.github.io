0b239d2fb5c2d602e8e0018e3f6054e4ffd10079
Cihat Aydın <cihataydin.tr@gmail.com>
2026-04-04 22:12:08 +0300

    chore:edits

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/cihat/Development/cihataydin.github.io/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/cihat/Development/cihataydin.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/cihat/Development/cihataydin.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/cihat/Development/cihataydin.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/cihat/Development/cihataydin.github.io/src/css/custom.css"),
-];
