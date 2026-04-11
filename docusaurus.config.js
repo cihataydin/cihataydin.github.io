350526b648de3ed9e3b396753257d3ac6eb9b780
Cihat Aydın <cihataydin.tr@gmail.com>
2026-04-11 17:36:07 +0300

    feat: edits dark light design

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,6 +72,8 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
         respectPrefersColorScheme: true,
       },
       navbar: {
